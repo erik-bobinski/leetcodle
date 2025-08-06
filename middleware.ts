4f0867e6902a24a406c57ab0b1d9d6ea38b78f33
erik-bobinski <ebobinski0@gmail.com>
2025-08-06 17:15:53 -0400

    i think i properly excluded the clerk webhook this time, i also excluded the ai cron since it uses its own secret

--- middleware.ts
+++ middleware.ts
@@ -20,8 +20,9 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes, except for clerk webhook
-    "/(api|trpc)(?!.*webhooks/clerk)(.*)"
+    // Skip /api/webhooks/clerk
+    "/((?!api/webhooks/clerk|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)"
   ]
 };
